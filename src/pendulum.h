f915c2d6c4f10cca36570b4958d2230644415825
ttj <taylor.johnson@gmail.com>
2015-06-22 23:17:11 -0400

    first version of real time reachability; version copied from https://bitbucket.org/verivital/rtss2014

---
+++ src/pendulum.h
@@ -0,0 +1,17 @@
+// Stanley Bak
+// Pendulum Real-time reach check header
+
+#ifndef PENDULUM_H_
+#define PENDULUM_H_
+
+#include <stdbool.h>
+#include "geometry.h"
+#include "main.h"
+
+// return the potential of the lmi-outputted function for a given state
+REAL potential(REAL pos, REAL vel, REAL theta, REAL omega);
+
+// check if the passed-in state is provably safe
+int isSafe(int runtimeMs, REAL state[NUM_DIMS]);
+
+#endif
